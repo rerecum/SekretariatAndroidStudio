2f78ecceb780b421b10e31428441eaee1ce82ec2
rerecum <kacperkacperkacper132@gmail.com>
2022-01-07 23:32:45 +0100

    wpisanie kodu w ConnectionHelper

---
+++ app/src/main/java/com/example/sekretariat_sqlconnection/ConnectionHelper.java
@@ -0,0 +1,37 @@
+package com.example.sekretariat_sqlconnection;
+
+import android.os.StrictMode;
+import android.util.Log;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionHelper {
+    Connection con;
+    String uname, pass, ip, port, database;
+
+    public Connection connectionclass()
+    {
+        ip = "192.168.1.26";
+        database="dbAppSekretariatDataSet";\
+        uname="sa";
+        pass="123";
+
+        StrictMode.ThreadPolicy policy= new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection connection= null;
+        String ConnectionURL = null;
+
+        try
+        {
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            ConnectionURL = "jdbc:jtds:sqlserver://"+ip+":"+port+";"+"databasename="+database+";user="+uname+";password="+pass+";";
+            connection = DriverManager.getConnection(ConnectionURL);
+        }
+        catch (Exception ex){
+            Log.e("Error ",ex.getMessage());
+        }
+
+        return connection;
+    }
+}
