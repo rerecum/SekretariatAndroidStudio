988566615398917d4f9d03a8953a80208c6d4be7
rerecum <kacperkacperkacper132@gmail.com>
2022-01-07 23:36:50 +0100

    dodanie kodu w MainActivity, public void

--- app/src/main/java/com/example/sekretariat_sqlconnection/MainActivity.java
+++ app/src/main/java/com/example/sekretariat_sqlconnection/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.sekretariat_sqlconnection;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.sql.Connection;
 
 public class MainActivity extends AppCompatActivity {
 
+    Connection connect;
+    String ConnectionResult="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void GetTextFromSQL(View v)
+    {
+        TextView tx1=(TextView) findViewById(R.id.textView3);
+        TextView tx2=(TextView) findViewById(R.id.textView4);
+    }
+
 }
